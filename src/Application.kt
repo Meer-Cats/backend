c615af2cb3ec1de44cfa7d1d2aca02dc4d47eb12
Enzo Mallard <enzo@mallard.ml>
2019-11-26 14:54:58 +0100

    feat(init): Setup KTor

---
+++ src/Application.kt
@@ -0,0 +1,83 @@
+package fr.prayfortalent
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.content.*
+import io.ktor.http.content.*
+import io.ktor.locations.*
+import io.ktor.sessions.*
+import io.ktor.gson.*
+import io.ktor.features.*
+import io.ktor.client.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.tomcat.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(Locations) {
+    }
+
+    install(Sessions) {
+        cookie<MySession>("MY_SESSION") {
+            cookie.extensions["SameSite"] = "lax"
+        }
+    }
+
+    install(ContentNegotiation) {
+        gson {
+        }
+    }
+
+    val client = HttpClient() {
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        // Put Angular Here
+        static("/static") {
+            resources("static")
+        }
+
+        get<MyLocation> {
+            call.respondText("Location: name=${it.name}, arg1=${it.arg1}, arg2=${it.arg2}")
+        }
+        // Register nested routes
+        get<Type.Edit> {
+            call.respondText("Inside $it")
+        }
+        get<Type.List> {
+            call.respondText("Inside $it")
+        }
+
+        get("/session/increment") {
+            val session = call.sessions.get<MySession>() ?: MySession()
+            call.sessions.set(session.copy(count = session.count + 1))
+            call.respondText("Counter is ${session.count}. Refresh to increment.")
+        }
+
+        get("/json/gson") {
+            call.respond(mapOf("hello" to "world"))
+        }
+    }
+}
+
+@Location("/location/{name}")
+class MyLocation(val name: String, val arg1: Int = 42, val arg2: String = "default")
+
+@Location("/type/{name}") data class Type(val name: String) {
+    @Location("/edit")
+    data class Edit(val type: Type)
+
+    @Location("/list/{page}")
+    data class List(val type: Type, val page: Int)
+}
+
+data class MySession(val count: Int = 0)
+
